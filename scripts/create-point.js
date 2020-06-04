7333e2c2f529b9fb96af0f485d471d3246477ac9
Flávio Félix <fau_felix30@hotmail.com>
2020-06-03 21:00:29 -0300

    Terminando a segunda a página e falta aplicar as funcionalidades

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -14,7 +14,7 @@ populateUfs()
 
 function getCities(event){
   const citySelect = document.querySelector("[name=city]")
-  const stateInput = document.input("[name=state]")
+  const stateInput = document.querySelector("[name=state]")
 
   const ufValue = event.target.value
 
@@ -23,11 +23,15 @@ function getCities(event){
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios
   `
+  citySelect.innerHTML = "<option value>Selecione a cidade</option>"
+  citySelect.disabled = true
+
   fetch(url)
   .then(res => res.json())
   .then( cities => {
+    
     for(const city of cities){
-      citySelect.innerHTML +=  `<option value='${city.id}'>${city.nome}</option>`
+      citySelect.innerHTML +=  `<option value='${city.nome}'>${city.nome}</option>`
     }
     citySelect.disabled = false
     
@@ -38,3 +42,50 @@ function getCities(event){
 document
   .querySelector("select[name=uf]")
   .addEventListener("change", getCities)
+
+  // Items de coleta
+  // pegar todos os li's
+  const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+  for(const item of itemsToCollect){
+    item.addEventListener("click", handleSelectedItem)
+
+  }
+
+  let selectedItems = [1, 2, 3, 4, 5, 6]
+
+  function handleSelectedItem(event){
+    const itemLi = event.target
+    // adicionar ou remover uma classe no javascript
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    //verificar se existem items selecionados, se sim
+    //pegar os itens selecionados
+    const alreadySelected = selectedItems.findIndex(function(){
+      const itemFound = item == itemId
+      return itemFound
+    })
+
+    //se já estiver selecionado, tirar a seleção.
+
+    //se não tiver selecionado, adicionar a seleção
+
+    //atualizar o campo escondido com os dados selecionados
+
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
